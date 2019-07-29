49ebfde927996fa89a9032532ed4e627ccfe1522
George Roubie <george.roubie@gmail.com>
2019-07-29 17:06:47 +0300

    Create css-custom-properties.service.ts

---
+++ css-custom-properties.service.ts
@@ -0,0 +1,21 @@
+// Angular Modules
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class CssVariablesService {
+
+	public getValue(propertyName: string): string | null {
+		try {
+			const root: HTMLElement = document.documentElement;
+			const value: string = getComputedStyle(root).getPropertyValue(propertyName);
+			return value ? value : null;
+		} catch(ex) { return null; }
+	}
+
+	public setValue(propertyName: string, newValue: string): void {
+		try {
+			const root: HTMLElement = document.documentElement;
+			root.style.setProperty(propertyName, newValue);
+		} catch (ex) { }
+	}
+}
